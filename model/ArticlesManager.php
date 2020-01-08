811a625f212ee9132badb91c17a30f350770e57b
Flo <flo@MacBook-Pro-de-Flo.local>
2020-01-08 15:28:17 +0100

    UsersManager finished

--- model/ArticlesManager.php
+++ model/ArticlesManager.php
@@ -2,26 +2,44 @@
 namespace Blog\model;
 use Blog\model\Manager;
 
+require_once 'Manager.php';
 
-require 'Manager.php';
 class ArticlesManager extends Manager 
 {
 
+    /**
+     * __construct --> Initialise DB when class is instanciate
+     *
+     * @return void
+     */
     public function __construct()
     {
         $this->db = $this->dbConnect();
     }
 
+    /**
+     * getPosts -> get all posts in DB
+     *
+     * @return array
+     */
     public function getPosts() 
     {
         $articles = [];
         $req = $this->db->query('SELECT * FROM Articles ORDER BY date_article DESC');
-        while ($data = $req->fetch(\PDO::FETCH_ASSOC)) {
+        while ($data = $req->fetch(\PDO::FETCH_ASSOC)) 
+        {
             $articles[] = $data;
         }
         return $articles;
     }
 
+    /**
+     * getOnePost -> select a post in DB
+     *
+     * @param  mixed $id
+     *
+     * @return array
+     */
     public function getOnePost($id)
     {
         $req = $this->db->prepare('SELECT * FROM Articles WHERE id = :id');
@@ -30,6 +48,14 @@ class ArticlesManager extends Manager
         return $article; 
     }
 
+    /**
+     * createPost
+     *
+     * @param  mixed $title
+     * @param  mixed $content
+     *
+     * @return void
+     */
     public function createPost($title, $content)
     {
         $req = $this->db->prepare('INSERT INTO Articles (title_article, content_article, date_article) VALUES(:title, :content, NOW())');
@@ -39,12 +65,28 @@ class ArticlesManager extends Manager
         ));
     }
 
+    /**
+     * deletePost
+     *
+     * @param  mixed $id
+     *
+     * @return void
+     */
     public function deletePost($id) 
     {
         $req = $this->db->prepare('DELETE FROM Articles WHERE id = :id');
         $req->execute(array('id' => $id));
     }
 
+    /**
+     * updatePost
+     *
+     * @param  mixed $id
+     * @param  mixed $title
+     * @param  mixed $content
+     *
+     * @return void
+     */
     public function updatePost($id, $title, $content)
     {
         $req = $this->db->prepare('UPDATE Articles SET title_article = :title, content_article = :content WHERE id = :id'); 
---
+++ model/UsersManager.php
@@ -0,0 +1,143 @@
+<?php 
+namespace Blog\model;
+use Blog\model\Manager;
+
+require_once 'Manager.php';
+
+class UsersManager extends Manager 
+{
+
+    /**
+     * __construc -> call DB when class is Instanciate
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        $this->db = $this->dbConnect();
+    }
+
+    /**
+     * getUsers -> list of all users
+     *
+     * @return array 
+     */
+    public function getUsers()
+    { 
+        $users = [];
+        $req = $this->db->query('SELECT * FROM Users ORDER BY id ASC');
+        while ($data = $req->fetch(\PDO::FETCH_ASSOC))
+        {
+            $users[] = $data;
+        }
+        var_dump($users);
+        return $users;
+    }
+
+    /**
+     * getOneUser -> select an user with his ID
+     *
+     * @param  mixed $id
+     *
+     * @return array
+     */
+    public function getOneUser($id)
+    {
+        $req = $this->db->prepare('SELECT * FROM Users WHERE id = :id');
+        $req->execute(array('id' => $id));
+        $user = $req->fetch(\PDO::FETCH_ASSOC);
+        return $user; 
+    }
+
+    /**
+     * addUser -> add an user in the DB
+     *
+     * @param  mixed $pseudo
+     * @param  mixed $password
+     * @param  mixed $mail
+     * @param  mixed $user_role
+     *
+     * @return void
+     */
+    public function addUser($pseudo, $password, $mail)
+    {
+        $req = $this->db->prepare('INSERT INTO Users (pseudo, password, mail) VALUES (:pseudo, :password, :mail)');
+        $req->execute(array(
+            'pseudo' => $pseudo,
+            'password' => $password, 
+            'mail' => $mail
+        ));
+    }
+
+    /**
+     * updateUser -> update user informations 
+     *
+     * @param  mixed $id
+     * @param  mixed $pseudo
+     * @param  mixed $password
+     * @param  mixed $mail
+     *
+     * @return void
+     */
+    public function updateUser($id, $pseudo, $password, $mail) 
+    {
+        $req = $this->db->prepare('UPDATE Users SET pseudo = :pseudo, password = :password, mail = :mail WHERE id = :id'); 
+        $req->execute(array(
+            'id' => $id,
+            'pseudo' => $pseudo,
+            'password' => $password,
+            'mail' => $mail
+        ));
+    }
+
+    /**
+     * delUser -> delete user with his ID
+     *
+     * @param  mixed $id
+     *
+     * @return void
+     */
+    public function delUser($id)
+    {
+        $req = $this->db->prepare('DELETE FROM Users WHERE id = :id');
+        $req->execute(array('id' => $id));
+    }
+
+
+    /**
+     * addAdmin -> change user_role to "admin"
+     *
+     * @param  mixed $id
+     *
+     * @return void
+     */
+    public function addAdmin($id)
+    {
+        $req = $this->db->prepare('UPDATE Users SET user_role = :adminRole WHERE id = :id');
+        $req->execute(array(
+            'adminRole' => 'admin',
+            'id' => $id
+        ));
+    }
+
+    /**
+     * delAdmin -> change user_role to "user"
+     *
+     * @param  mixed $id
+     *
+     * @return void
+     */
+    public function delAdmin($id)
+    {
+        $req = $this->db->prepare('UPDATE Users SET user_role = :adminRole WHERE id = :id');
+        $req->execute(array(
+            'adminRole' => 'user',
+            'id' => $id
+        ));
+    }
+
+}
+
+$test = new UsersManager();
+$test->getUsers();
+$test->updateUser(4, 'rarara', 'rarara', 'rarara');
