32aa87c3b55430fe9a237ae6df8cb8dbbd601351
Flo <flo@MacBook-Pro-de-Flo.local>
2020-02-10 15:24:18 +0100

    Delete informations PDO before push

--- model/Manager.php
+++ model/Manager.php
@@ -11,7 +11,6 @@ abstract class Manager {
     protected function dbConnect() {
         try {
             $db = new \PDO('mysql:host=localhost;dbname=Blog;charset=utf8', 'root', 'root', array(\PDO::ATTR_ERRMODE => \PDO::ERRMODE_EXCEPTION));
-         // $db = new \PDO('mysql:host=db5000283097.hosting-data.io;port=3306;dbname=dbs276326;charset=utf8', 'dbu486824', '@Xx6y3p33', array(\PDO::ATTR_ERRMODE => \PDO::ERRMODE_EXCEPTION));
             return $db;
         }
         catch (\Exception $e) {
